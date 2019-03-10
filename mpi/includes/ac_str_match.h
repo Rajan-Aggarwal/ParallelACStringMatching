1469cf112bdf0fe973f77b66ce11ef00017acf92
Rajan Aggarwal <rajansien@gmail.com>
2019-03-10 20:53:00 +0530

    Edit comments

--- mpi/includes/ac_str_match.h
+++ mpi/includes/ac_str_match.h
@@ -3,6 +3,6 @@
 
 #define MAX_STR_LEN 100
 
-void detect_patterns(struct trie_node *root, char *input, int start); //outputs all the matched patterns to stdout
+void detect_patterns(struct trie_node *root, char *input, int start); // outputs all the matched patterns to stdout
 
 #endif /* __AC_STR_MATCH_H */
