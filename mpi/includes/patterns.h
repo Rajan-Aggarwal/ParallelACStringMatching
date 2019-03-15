87064a0b1d46f06a300f62ec507a37be8ec03478
Rajan Aggarwal <rajansien@gmail.com>
2019-03-16 02:09:34 +0530

    Add patterns header file

---
+++ mpi/includes/patterns.h
@@ -0,0 +1,30 @@
+#ifndef __PATTERNS_H
+#define __PATTERNS_H
+
+/**
+*	File consists of pattern list that are percieved threats for an IDS:
+*		1. Reverse shell code
+*		2. Reference to /bin/sh
+*		3. Any executable
+**/
+
+const char *pattern_list[] = {
+	// 1.
+	"bash -i >& /dev/tcp/",
+	"subprocess.call",
+	"sprintf",
+	"exec",
+	"nc",
+
+	// 2.
+	"/bin",
+	"/sh",
+
+	// 3.
+	".pl",
+	".php",
+	".py",
+	".sh"
+}
+
+#endif /* __PATTERNS_H */
